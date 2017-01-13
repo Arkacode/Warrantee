07db5a88478d5fdfa9c9e99bfe468ae4f1b3389e
Sting <skater.rafa96@gmail.com>
2017-01-13 20:16:42 +0000

    login fb fix

--- app/src/main/java/pt/ipca/cm/warrantee/LoginActivity.java
+++ app/src/main/java/pt/ipca/cm/warrantee/LoginActivity.java
@@ -1,5 +1,6 @@
 package pt.ipca.cm.warrantee;
 
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,8 +32,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -42,56 +44,54 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setReadPermissions(Arrays.asList(
                 "public_profile", "email", "user_birthday", "user_friends"));// get info
 
+        loginButton.registerCallback(callbackManager, callback);
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                AccessToken accessToken = loginResult.getAccessToken();
-                Profile profile = Profile.getCurrentProfile();
+    }
 
-                // Facebook Email address
-                GraphRequest request = GraphRequest.newMeRequest(
-                        loginResult.getAccessToken(),
-                        new GraphRequest.GraphJSONObjectCallback() {
-                            @Override
-                            public void onCompleted(
-                                    JSONObject object,
-                                    GraphResponse response) {
-                                Log.v("LoginActivity Response ", response.toString());
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        public void onSuccess(LoginResult loginResult) {
+            // App code
+            GraphRequest request = GraphRequest.newMeRequest(
+                    loginResult.getAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+                            Log.v("LoginActivity", response.toString());
 
-                                try {
-                                    String Name = object.getString("name");
-
-                                    String FEmail = object.getString("email");
-                                    Log.v("Email = ", " " + FEmail);
-                                    Toast.makeText(getApplicationContext(), "Name " + Name, Toast.LENGTH_LONG).show();
-
-
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
+                            // Application code
+                            try {
+                                String email = object.getString("email");
+                                String birthday = object.getString("birthday");// 01/31/1980 format
+                                Toast.makeText(getApplicationContext(),"Email : " + email,Toast.LENGTH_LONG).show();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                        });
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender, birthday");
-                request.setParameters(parameters);
-                request.executeAsync();
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,email,gender,birthday");
+            request.setParameters(parameters);
+            request.executeAsync();
 
 
-            }
+        }
 
-            @Override
-            public void onCancel() {
-                LoginManager.getInstance().logOut();
-            }
+        @Override
+        public void onCancel() {
+            // App code
+            Log.v("LoginActivity", "cancel");
+        }
 
-            @Override
-            public void onError(FacebookException e) {
-                Toast.makeText(getApplicationContext(),"Erro", Toast.LENGTH_SHORT).show();
-            }
-        });
+        @Override
+        public void onError(FacebookException exception) {
+            // App code
+            Log.v("LoginActivity", exception.getCause().toString());
+        }
+    };
 
-
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
