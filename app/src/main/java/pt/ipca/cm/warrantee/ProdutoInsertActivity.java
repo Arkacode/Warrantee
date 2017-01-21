befe524381d40a5f4e9434d8c0aef923d4a14e33
krozs <rodolfomota96@gmail.com>
2017-01-21 02:37:05 +0000

    scan

--- app/src/main/java/pt/ipca/cm/warrantee/ProdutoInsertActivity.java
+++ app/src/main/java/pt/ipca/cm/warrantee/ProdutoInsertActivity.java
@@ -3,12 +3,50 @@ package pt.ipca.cm.warrantee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import pt.ipca.cm.warrantee.com.google.zxing.integration.android.IntentIntegrator;
+import pt.ipca.cm.warrantee.com.google.zxing.integration.android.IntentResult;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProdutoInsertActivity extends AppCompatActivity {
-
+public class ProdutoInsertActivity extends Activity implements OnClickListener{
+    private Button scanBtn;
+    private TextView editTextScanSerial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto_insert);
+        scanBtn = (Button)findViewById(R.id.buttonScan);
+        editTextScanSerial = (TextView)findViewById(R.id.editTextScan);
+        scanBtn.setOnClickListener(this);
+
+    }
+    public void onClick(View v){
+        //respond to clicks
+        if(v.getId()==R.id.buttonScan){
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+//we have a result
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            /*formatTxt.setText("FORMAT: " + scanFormat);*/
+            editTextScanSerial.setText("CONTENT: " + scanContent);
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
