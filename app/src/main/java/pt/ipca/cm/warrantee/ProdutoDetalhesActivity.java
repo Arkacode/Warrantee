788ad1adab339d0bb56676cf27261863177af8c9
Sting <skater.rafa96@gmail.com>
2017-01-29 22:39:44 +0000

    Date Picker and some minor validations

--- app/src/main/java/pt/ipca/cm/warrantee/ProdutoDetalhesActivity.java
+++ app/src/main/java/pt/ipca/cm/warrantee/ProdutoDetalhesActivity.java
@@ -1,11 +1,13 @@
 package pt.ipca.cm.warrantee;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import com.facebook.Profile;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 import pt.ipca.cm.warrantee.Model.Moeda;
@@ -52,6 +55,7 @@ public class ProdutoDetalhesActivity extends AppCompatActivity  implements View.
     String categoria;
     String nomeProduto;
     String marca;
+    Calendar myCalendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,6 +75,33 @@ public class ProdutoDetalhesActivity extends AppCompatActivity  implements View.
         editTextLocalCompra = (EditText) findViewById(R.id.editTextLocalCompra);
         editTextDataCompra = (EditText) findViewById(R.id.editTextDataCompra);
         editTextPreco = (EditText) findViewById(R.id.editTextPreco);
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        editTextDataCompra.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(ProdutoDetalhesActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
 
         textViewNomeProduto.setText(nomeProduto);
         textViewMarcaProduto.setText(marca);
@@ -104,19 +135,14 @@ public class ProdutoDetalhesActivity extends AppCompatActivity  implements View.
             }
         });
     }
+
+    private void updateLabel() {
+        String myFormat = "yyyy-MM-dd"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        editTextDataCompra.setText(sdf.format(myCalendar.getTime()));
+    }
     public void onClick(View v) {
         if (v.getId() == R.id.buttonConfimarDetalhes) {
-
-            /*produto = new Produto();
-            produto.setCodigoBarras(codBarras);
-            produto.setNome(nomeProduto);
-            produto.setSerialNumber(codSerie);
-            produto.setMarca(marca);
-            produto.setPeriodo(editTextPeriodoGarantia.getText().toString());
-            produto.setFornecedor(editTextFornecedor.getText().toString());
-            produto.setLocalCompra(editTextLocalCompra.getText().toString());
-            produto.setDataCompra(editTextDataCompra.getText().toString());
-            produto.setPreco(editTextPreco.getText().toString());*/
             data.putString("periodo",editTextPeriodoGarantia.getText().toString());
             data.putString("fornecedor",editTextFornecedor.getText().toString());
             data.putString("localCompra",editTextLocalCompra.getText().toString());
